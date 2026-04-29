b6077da8dab957b7a11317cdc8951764877372d1
joseluisitograu-blip <joseluisitograu@gmail.com>
2026-04-29 15:20:03 +0200

    Create robots.tsx

---
+++ public/robots.tsx
@@ -0,0 +1,13 @@
+User-agent: *
+Allow: /
+Allow: /w/
+Allow: /auth
+Allow: /privacidad
+Allow: /terminos
+Allow: /reembolso
+Allow: /cookies
+
+Disallow: /dashboard
+Disallow: /dashboard/edit/
+
+Sitemap: https://bodasfacil.com/sitemap.xml
